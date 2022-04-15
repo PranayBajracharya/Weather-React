623a3f089d1d8ca2ea217ae9560518de1d68556f
PranayBajracharya <law.pran99@gmail.com>
2022-04-15 14:03:44 +0545

    feat: search city on change

--- src/components/SelectCity/SelectCity.js
+++ src/components/SelectCity/SelectCity.js
@@ -1,49 +1,78 @@
+import { useState, useEffect, useRef } from "react";
 import {
     FormControl,
+    FormLabel,
     UnorderedList,
-    Button,
     Input,
     Flex,
     Text,
 } from "@chakra-ui/react";
+import { BsSearch } from "react-icons/bs";
 
-import { useState } from "react";
 import CityList from "./CityList";
 
 const SelectCity = (props) => {
     const [searchValue, setSearchValue] = useState("");
     const [cityOptions, setCityOptions] = useState([]);
-    const [error, setError] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
+
+    const controller = useRef(null);
 
     const searchValueHandler = (event) => {
+        controller.current && controller.current.abort();
         setSearchValue(event.target.value);
-        setError(false);
     };
 
-    const searchCityHandler = async (event) => {
+    useEffect(() => {
+        const searchCityHandler = async () => {
+            try {
+                setIsLoading(true);
+                if (searchValue.trim() === "") {
+                    setCityOptions([]);
+                    return;
+                }
+                controller.current = new AbortController();
+    
+                let cities = await fetch(
+                    `http://api.openweathermap.org/geo/1.0/direct?q=${searchValue}&limit=5&appid=3d4fe1331b4eeb7f11a9e53fa3b7fa36`,
+                    {
+                        method: "GET",
+                        signal: controller.current.signal,
+                    }
+                );
+                console.log(cities);
+    
+                cities = await cities.json();
+                setCityOptions(cities);
+                document.getElementById("searchResults").style.display = "block";
+            } catch (error) {}
+            setIsLoading(false);
+        };
+        searchCityHandler();
+    }, [searchValue]);
+
+    //function for search city submit/enter
+    const setCity = (event) => {
         event.preventDefault();
-        if (searchValue.trim() === "") {
-            return;
-        }
-        try {
-            let cities = await fetch(
-                `http://api.openweathermap.org/geo/1.0/direct?q=${searchValue}&limit=5&appid=3d4fe1331b4eeb7f11a9e53fa3b7fa36`
-            );
-            cities = await cities.json();
-            setCityOptions(cities);
-            document.getElementById("searchResults").style.display = "block";
-            setError(false);
-        } catch (error) {
-            setError(true);
+
+        if (cityOptions.length > 0) {
+            props.setCity({
+                name: cityOptions[0].name,
+                country: cityOptions[0].country,
+                lat: cityOptions[0].lat,
+                lon: cityOptions[0].lon,
+            });
+            setSearchValue("");
         }
     };
 
+    //list of cities search result
     let options;
     if (cityOptions.length > 0) {
-        options = cityOptions.map((city) => {
+        options = cityOptions.map((city, index) => {
             return (
                 <CityList
-                    key={city.lat}
+                    key={index}
                     name={city.name}
                     country={city.country}
                     lat={city.lat}
@@ -54,11 +83,10 @@ const SelectCity = (props) => {
         });
     }
 
-
     return (
         <Flex direction="column" position="relative">
-            <form onSubmit={searchCityHandler}>
-                <FormControl display="flex">
+            <form onSubmit={setCity}>
+                <FormControl display="flex" alignItems="center">
                     <Input
                         type="search"
                         className="dropdown"
@@ -87,21 +115,22 @@ const SelectCity = (props) => {
                             {options}
                         </UnorderedList>
                     )}
-                    <Button type="submit" ml={2}>
-                        Search
-                    </Button>
+                    <FormLabel fontSize="20px" margin="0 0 0 10px">
+                        <BsSearch />
+                    </FormLabel>
                 </FormControl>
             </form>
-            {error && (
-                <Text
-                    position="absolute"
-                    top="100%"
-                    fontSize="14px"
-                    color="FFFFFFF"
-                >
-                    Not Found! Enter city name precisely.
-                </Text>
-            )}
+            <Text
+                position="absolute"
+                top="100%"
+                fontSize="14px"
+                color="FFFFFFF"
+            >
+                {!isLoading &&
+                    cityOptions.length === 0 &&
+                    searchValue.trim() !== "" &&
+                    "No cities found! Enter city name precisely!"}
+            </Text>
         </Flex>
     );
 };
